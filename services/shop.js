4c37053f5c08fe4f92d99abe724351cfc47f16bc
ohmpr <nanoic_ohm@hotmail.com>
2020-01-14 16:39:18 +0700

    search

--- services/shop.js
+++ services/shop.js
@@ -1,13 +1,34 @@
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op
+
 module.exports = (app, db) => {
 
-    app.get('/shops', (req, res) => {
-        db.shop.findAll()
-            .then((result) => {
-                res.status(201).json(result)
-            })
-            .catch((err) => {
-                res.status(400).json()
-            })
+  app.get('/shops', (req, res) => {
+    db.shop.findAll()
+      .then((result) => {
+        res.status(201).json(result)
+      })
+      .catch((err) => {
+        res.status(400).json()
+      })
+  })
+  // req.params.keyword
+  app.get('/searchShop', (req, res) => {
+    db.shop.findAll({
+      offset: (req.query.page - 1) * 3,
+      limit: 3,
+      where: {
+        shopName: {
+          [Op.substring]: `%${req.query.keyword}`
+        }
+      },
     })
+      .then((result) => {
+        res.status(200).json(result)
+      })
+      .catch((err) => {
+        res.status(404).json()
+      })
+  })
 
 }
