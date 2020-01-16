084e5cd2a6af1a2207bd8cf9859c0d40bd779eb0
Apichai123 <rusefer@gmail.com>
2020-01-16 14:06:40 +0700

    fix error

--- services/shop.js
+++ services/shop.js
@@ -78,69 +78,72 @@ module.exports = (app, db) => {
         .catch(err => console.error(err));
     }
   );
+
+  app.get("/searchshops", (req, res) => {
+    db.shop
+      .findAll({
+        include: [
+          {
+            model: db.shopPic,
+            attributes: ["picName"]
+          }
+        ],
+        limit: 3,
+        offset: (req.query.page - 1) * 3,
+        where: {
+          shopName: {
+            [Op.substring]: `%${req.query.keyword}`
+          }
+        }
+      })
+      .then(result => {
+        res.status(200).json(result);
+      })
+      .catch(err => {
+        res.status(400).json();
+      });
+  });
+  
+  app.get("/shops/:sid", (req, res) => {
+    db.shop
+      .findAll({
+        include: [
+          {
+            model: db.shopPic,
+            attributes: ["picName"]
+          }
+        ],
+        limit: 3,
+        offset: (req.params.sid - 1) * 3
+      })
+      .then(result => {
+        res.status(201).json(result);
+      })
+      .catch(err => {
+        res.status(400).json();
+      });
+  });
+  
+  
+  app.get("/shop", (req, res) => {
+    db.shop
+      .findOne({
+        where: { id: req.query.id },
+        include: [
+          {
+            model: db.service
+          }
+        ]
+      })
+      .then(result => {
+        res.status(200).json(result);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  });
+  
+
 };
 
 
-app.get("/searchshops", (req, res) => {
-  db.shop
-    .findAll({
-      include: [
-        {
-          model: db.shopPic,
-          attributes: ["picName"]
-        }
-      ],
-      limit: 3,
-      offset: (req.query.page - 1) * 3,
-      where: {
-        shopName: {
-          [Op.substring]: `%${req.query.keyword}`
-        }
-      }
-    })
-    .then(result => {
-      res.status(200).json(result);
-    })
-    .catch(err => {
-      res.status(400).json();
-    });
-});
-
-app.get("/shops/:sid", (req, res) => {
-  db.shop
-    .findAll({
-      include: [
-        {
-          model: db.shopPic,
-          attributes: ["picName"]
-        }
-      ],
-      limit: 3,
-      offset: (req.params.sid - 1) * 3
-    })
-    .then(result => {
-      res.status(201).json(result);
-    })
-    .catch(err => {
-      res.status(400).json();
-    });
-});
-
-
-app.get("/shop", (req, res) => {
-  db.shop
-    .findOne({
-      where: { id: req.query.id },
-      include: [
-        {
-          model: db.service
-        }
-      ]
-    })
-    .then(result => {
-      res.status(200).json(result);
-    })
-    .catch(err => {
-      res.status(400).json(err);
-    });
-});
