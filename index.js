f2c5a417f936145af34fe8db58a2125d559241c5
atiwatice <48309112+atiwatice@users.noreply.github.com>
2020-01-17 09:46:41 +0700

    fix conflic history statement

--- index.js
+++ index.js
@@ -25,16 +25,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 require("./config/passport/passport");
 
-db.sequelize.sync({ alter: true }).then(() => {
+db.sequelize.sync({ force: true }).then(() => {
   shopService(app, db);
   sericeService(app, db);
   historyStatement(app,db)
   userService(app, db);
   shopPicService(app, db);
 
-  historyStatementService(app,db)
 
-  historyStatementService(app,db)
+
 
   app.get('/protected', passport.authenticate('jwt', { session: false }),
     function (req, res) {
