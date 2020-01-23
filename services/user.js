0baaf1a6cd4eb01f3a08703fcde39a34039f96e1
Bhornumnard <tak_25908@hotmail.com>
2020-01-23 15:11:07 +0700

    update database

--- services/user.js
+++ services/user.js
@@ -102,6 +102,28 @@ module.exports = (app, db) => {
         })
     })
 
+  app.get(
+    "/Alluser", (req, res) => {
+      db.user.findAll()
+        .then(result => {
+          res.status(201).json(result);
+        })
+        .catch(err => {
+          res.status(400).json();
+        });
+
+    })
+
+  app.delete("/deleteUser/:id", (req, res) => {
+    db.user.destroy({ where: { id: req.params.id } }).then(result => {
+      console.log('delete complete')
+      res.status(201).json(result)
+    }).catch(err => {
+      console.log('cannot delete')
+      res.status(400).json(err.message);
+    });
+  })
+
 }
 
 
