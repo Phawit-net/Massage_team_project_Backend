f45fa814b1200acaf14c107de0c1f942fc5e536b
Apichai123 <rusefer@gmail.com>
2020-01-24 17:41:16 +0700

    edit config

--- services/shop.js
+++ services/shop.js
@@ -88,6 +88,7 @@ module.exports = (app, db) => {
           if (!req.files) {
           } else {
             const picture = req.files.photoPost;
+            //console.log(picture);
             const pictureName = `${shopName}/${new Date().getTime()}.jpeg`;
             await picture.mv(`./image/` + pictureName);
             // console.log(req.body.latitude, req.body.longitude)
