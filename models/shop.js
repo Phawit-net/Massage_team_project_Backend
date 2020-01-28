87e7ec850d852d0e658360100950930588ea21e4
bebiiez <natthalada.p@gmail.com>
2020-01-28 14:19:51 +0700

    edit seeder & service/shopBank

--- models/shop.js
+++ models/shop.js
@@ -1,35 +1,37 @@
 module.exports = (sequelize, DataTypes) => {
-    const shop = sequelize.define("shop", {
-      shopName: {
-        type: DataTypes.STRING(255)
-      },
-      shopDescription: {
-        type: DataTypes.TEXT
-      },
-      shopProfilePic:{
-          type: DataTypes.STRING(255)
-      },
-      shopAccountNo:{
-          type:DataTypes.STRING(255)
-      },
-      shopAccountName:{
-          type:DataTypes.STRING(255)
-      },
-      shopBank:{
-         type:DataTypes.STRING(255)
-      }
+  const shop = sequelize.define("shop", {
+    shopName: {
+      type: DataTypes.STRING(255)
+    },
+    shopDescription: {
+      type: DataTypes.TEXT
+    },
+    shopProfilePic: {
+      type: DataTypes.STRING(255)
+    },
+    shopBank: {
+      type: DataTypes.STRING(50)
+    },
+    shopAccountNo: {
+      type: DataTypes.STRING(255)
+    },
+    shopAccountName: {
+      type: DataTypes.STRING(255)
+    },
+    shopBank: {
+      type: DataTypes.STRING(255)
+    }
+  });
+
+  shop.associate = models => {
+    shop.hasMany(models.shopPic, { onDelete: "CASCADE", foreignKey: "shop_id" });
+    shop.hasMany(models.service, { onDelete: "CASCADE", foreignKey: "shop_id" });
+    shop.hasOne(models.address, { onDelete: "CASCADE", foreignKey: "shop_id" });
+    shop.hasMany(models.historyStatement, {
+      onDelete: "CASCADE",
+      foreignKey: "shop_id"
     });
-  
-    shop.associate = models => {
-      shop.hasMany(models.shopPic, { onDelete: "CASCADE", foreignKey: "shop_id" });
-      shop.hasMany(models.service, { onDelete: "CASCADE", foreignKey: "shop_id" });
-      shop.hasOne(models.address, { onDelete: "CASCADE", foreignKey: "shop_id" });
-      shop.hasMany(models.historyStatement, {
-        onDelete: "CASCADE",
-        foreignKey: "shop_id"
-      });
-      
-    };
-    return shop
+
   };
-  
+  return shop
+};
